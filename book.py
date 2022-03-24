1ab3e39726f5389800eca3c5c59b1ac5a355a6c6
Dan Alvares <dan.alvares@gmail.com>
2022-03-24 10:01:56 -0300

    Extrai completamente a informação textual. Captura imagens e as guarda numa lista, que serão inseridas posteriormente através de um loop no material principal, buscando for 'fig' in x. Ainda não é ideal, mas já é um avanço.

--- book.py
+++ book.py
@@ -10,28 +10,33 @@ with open('teste.html', 'r') as arquivo_html:
     conteudo = arquivo_html.read()
 
 registro_conteudo = []
+registro_imagens = []
 soup = bs(conteudo, 'lxml')
 titulo_capitulo = soup.find(class_='entry-title')  # raspa o título do conteúdo da página
 registro_conteudo.append(titulo_capitulo.text)
 conteudo_capitulo = soup.find(class_='entry-content')  # raspa o conteúdo textual e imagens contidas no texto da página
-
+imagens = conteudo_capitulo.find_all('img')
+for imagem in imagens:
+    dados_imagem = imagem['alt'], imagem['src']
+    registro_imagens.append(dados_imagem)
 
 for parte in conteudo_capitulo:
     if parte.name == 'p':
-        # and parte.attrs['style'][0] == 'text-align: justify;':
-        if parte.find_next == 'img':
-            registro_conteudo.append(parte['src'])
-        else:
-            registro_conteudo.append(parte.text)
+        registro_conteudo.append(parte.text)
     else:
-        pass
+        continue
 
-# for i in registro_conteudo:
-#     print(i)
+for x in registro_conteudo:
+    contador = 0
+    teste = 'Fig' in x
+    if teste:
+        registro_conteudo[registro_conteudo.index(x)] = registro_imagens[contador]
+        contador += 1
+    else:
+        continue
 
-for i in registro_conteudo:
-    print(i)
-
+# for c in registro_conteudo:
+#     print(registro_conteudo.index(c), c)
 # proximo_conteudo = soup.find(class_='navigation post-navigation')
 #
 # proximo_link = proximo_conteudo.find(name='div', class_='nav-next')
